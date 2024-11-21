43de2b78c7f197b1179218a25c32d991fafe3929
tintin <singhnchn08@gmail.com>
2024-11-21 21:58:37 +0530

    initial dummy platform device driver

--- modules_practise/sample_platform_driver/dummy_platform.c
+++ modules_practise/sample_platform_driver/dummy_platform.c
@@ -10,12 +10,63 @@
 #define PLAT_DEVICE_FAKE_END_ADDRESS 	  	0x400
 
 
+static void platdevice_release(struct device *dev) {
+	printk("plat device release called \n");
+}
+
+static int platdriver_probe(struct platform_device *pdev) {
+	printk("plat driver probe invoked \n");
+	return 0;
+}
+
+static int platdriver_remove(struct platform_device *pdev) {
+	printk("plat driver remove invoked \n");
+	
+	return 0;
+}
+
+static struct resource plat_resource = {
+	.start = PLAT_DEVICE_FAKE_START_ADDRESS,
+	.end   = PLAT_DEVICE_FAKE_END_ADDRESS,
+	.flags = IORESOURCE_MEM,
+};
 
 
+static struct platform_device plat_device  = {
+		.name = "plat_device_example",
+		.id = -1,
+		.num_resources = 1,
+		.resource = &plat_resource,
+		.dev = {
+			.release = platdevice_release,
+		},
+};
+
+
+static const struct of_device_id myplat_id_table = { 
+	.compatible = "plat_device_example",
+};
+
+static struct platform_driver platdriver_struct = {
+	.probe = platdriver_probe,
+	.remove = platdriver_remove,
+	.driver = {
+		.name = "plat_device_example",
+		.of_match_table = &myplat_id_table,
+	},
+};
 
 static int __init plat_init(void) {
 
-	printk(KERN_INFO  "Registering a Fake Platform Device \n");
+	/*
+	  Registering a dummy device for which platform_driver_register can be invoked.
+	*/
+	printk(KERN_INFO  "Registering a Fake Platform Device \n");	
+	platform_device_register(&plat_device);
+	
+	/* Registering with Kernel */
+	printk(KERN_INFO "Registering platform driver - should invoke probe \n");
+	platform_driver_register(&platdriver_struct);
 
 	return 0;
 }
@@ -23,7 +74,13 @@ static int __init plat_init(void) {
 static void __exit plat_exit(void) {
 
 	printk(KERN_INFO  "UnRegistering the Fake Platform Device from Kernel \n");
-
+	
+	/* Unregistering the driver from Kernel */
+	platform_driver_unregister(&platdriver_struct);
+	
+	/* Unregistering the device */
+	platform_device_unregister(&plat_device);
+	
 	return;
 }
 
